10a30dbc084ef53d92f065ab77026757662ec2d9
javi <jalvarezotero@danielcastelao.org>
2019-03-19 23:08:19 +0100

    Acabado

---
+++ src/pruebaproyectob/ocupacion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaproyectob;
+
+/**
+ *
+ * @author javi
+ */
+public class ocupacion {
+  public static void metodo(){
+                   System.out.println("En Rio Bajo quedan un  total de 254 asientos disponibles");
+                   System.out.println("En Rio Alto quedan un total de 764 asientos disponibles");
+                   System.out.println("En Marcador no quedan asientos disponibles");
+                   System.out.println("En Tribuna quedan un total de 158 asientos disponibles");
+                   System.out.println("En Gol quedan un total de 221 asientos disponibles");
+   
+
+
+  }
+
+
+
+
+  
+}
